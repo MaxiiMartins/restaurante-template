4596c1acf5008d3341ef55a5a28fb19a4793e527
Maximiliano <maximartins45@gmail.com>
2023-07-07 23:46:23 -0300

    Redes sociales terminado

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,17 @@
+import { socialData } from "../data";
+
+const getSocials = (options) =>{
+  return (
+    options.map((item,i) =>(
+      <a key={i} className="border-2 rounded-full w-9 h-9 flex items-center justify-center text-sm hover:text-accent hover:border-accent transition-all durationn-300 " href={"#"}>{item.icon}</a>
+    ))
+  )
+}
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex gap-x-[10px]" >
+    {getSocials(socialData)}
+  </div>;
 };
 
 export default Socials;
