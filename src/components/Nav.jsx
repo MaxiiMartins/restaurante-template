234e4f17e916ee360990e55af1ab4f8a1f793a8f
Maximiliano <maximartins45@gmail.com>
2023-07-07 23:45:40 -0300

    Nav terminado

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,23 @@
+import { navData } from "../data";
+
+const getOptions = (options)=> {
+  return options.map((item,i)=>(
+    <li key={i}>
+      <a className="text-xl capitalize font-primary italic hover:text-dark transition-all durationn-300 " href={"#"}>{item.name}</a>
+    </li>
+  ))
+}
+ 
+
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="w-full h-full">
+      <ul className="h-full flex flex-col justify-center items-center gap-y-4" >
+      {getOptions(navData)}
+      </ul>
+    </nav>
+  )
 };
 
 export default Nav;
